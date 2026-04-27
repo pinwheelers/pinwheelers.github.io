e2699414bc0025d07acdf59bac4d1173009ff0d2
Parapet Cloud <parapetcloud@live.com>
2026-04-27 14:05:46 -0700

    fixed profile image scaling issue on narrow displays such as phones.

--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -1,16 +1,16 @@
 import type { JSX } from "react";
 import "./Profile.scss"
 
-interface IProfileProps{
-    image:string;
+interface IProfileProps {
+    image: string;
     children: any;
-    name:string;
+    name: string;
 }
 
-export default function Profile({children, image, name}: IProfileProps): JSX.Element {
-    return <div className = "profileContainer"> 
+export default function Profile({ children, image, name }: IProfileProps): JSX.Element {
+    return <div className="profileContainer">
         <img src={image} alt={`A picture of ${name}`} className="profileImageFormat" />
-        <div className = "profileText"> 
+        <div className="profileText">
             <h2>{name}</h2>
             {children}
         </div>
